0034a05a9e9c584f6cbe60a6baf935d7e39c6762
邵晨智的Macbook Pro 16寸 <shaochenzhi@shaochenzhideMacBook-Pro.local>
2024-02-24 22:33:19 +0800

    1.两数之和

---
+++ LeetCode1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    return 0;
+}
+
+
+// 双重for循环遍历
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> res;
+        unsigned long size = nums.size();
+        for (int i = 0; i < size; i++) {
+            for (int j = i + 1; j < size; j++) {
+                if (nums[i] + nums[j] == target) {
+                    res.push_back(i);
+                    res.push_back(j);
+                    return res;
+                }
+            }
+        }
+        return res;
+    }
+};
+
+// 通过哈希表记录每个元素及其位置
+class Solution_2 {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> res;
+        unordered_map<int, int> m;
+        for (int i = 0; i < nums.size(); i++) {
+            if (m.find(target - nums[i]) != m.end()) {
+                res.push_back(m[target - nums[i]]);
+                res.push_back(i);
+            } else {
+                m.insert(make_pair(nums[i], i));
+            }
+        }
+        return res;
+    }
+};
